85fd12fd8bb822e8f8e27af26f512c9a623fb6cd
sanddr <125254749+sanddr@users.noreply.github.com>
2023-03-08 16:14:25 +0800

    Add files via upload

---
+++ divide.js
@@ -0,0 +1,76 @@
+let option1 = document.getElementById("option1");
+let option2 = document.getElementById("option2");
+let option3 = document.getElementById("option3");
+let answer = 0;
+let audio = document.getElementById("myAudio");
+
+
+function generateEquation(){
+//Assign random numbers to make the equation
+    let num1 = Math.floor(Math.random() * 100 + 1);
+    console.log(num1);
+    let num2 = Math.floor(Math.random() * 10 + 1);
+    console.log(num2);
+
+
+//Calculate the correct answer
+if(num1%num2 === 0){
+    answer = num1 / num2;
+//Add randomly generated numbers to create the equation
+    document.getElementById("num1").innerHTML = num1;
+    document.getElementById("num2").innerHTML = num2;
+}else{
+    generateEquation();
+}
+    
+
+
+//Assign dummy anserws
+    let dummyAnswer1 = Math.floor(Math.random() * 11 + 1);
+    let dummyAnswer2 = Math.floor(Math.random() * 51 + 1);
+
+
+//array with all choices
+    let allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+    let switchAnswers = [];
+
+//shuffle possible answers
+//////////////// come back HERE////////////
+for(i = allAnswers.length; i--;){
+    switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i+1)), 1));
+}
+
+//Assign all possible answers in HTML
+    option1.innerHTML = switchAnswers[0];
+    option2.innerHTML = switchAnswers[1];
+    option3.innerHTML = switchAnswers[2];
+}
+
+
+//Add click function to every option and evaluate if the answer is correct
+option1.addEventListener("click", function(){
+    if(option1.innerHTML == answer){  //option1.innerHTML is a STRING so need to be ==, cannot use ===
+        generateEquation();
+    }else{
+        audio.play();
+    }
+})
+
+option2.addEventListener("click", function(){
+    if(option2.innerHTML == answer){
+        generateEquation();
+    }else{
+        audio.play();
+    }
+})
+
+option3.addEventListener("click", function(){
+    if(option3.innerHTML == answer){
+        generateEquation();
+    }else{
+        audio.play();
+    }
+})
+
+//Call the function
+generateEquation();
---
+++ subtract.js
@@ -0,0 +1,72 @@
+let option1 = document.getElementById("option1");
+let option2 = document.getElementById("option2");
+let option3 = document.getElementById("option3");
+let answer = 0;
+let audio = document.getElementById("myAudio");
+
+
+function generateEquation(){
+//Assign random numbers to make the equation
+    let num1 = Math.floor(Math.random() * 11);
+    let num2 = Math.floor(Math.random() * 11);
+
+//Calculate the correct answer (avoid negative integers)
+    if(num2 > num1){
+        answer = num2 - num1;
+        document.getElementById("num1").innerHTML = num2;
+        document.getElementById("num2").innerHTML = num1;
+    }else{
+    answer = num1 - num2;
+        document.getElementById("num1").innerHTML = num1;
+        document.getElementById("num2").innerHTML = num2;
+    }
+
+//Assign dummy anserws
+    let dummyAnswer1 = Math.floor(Math.random() * 21);
+    let dummyAnswer2 = Math.floor(Math.random() * 21);
+
+
+//array with all choices
+    let allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+    let switchAnswers = [];
+
+//shuffle possible answers
+//////////////// come back HERE////////////
+for(i = allAnswers.length; i--;){
+    switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i+1)), 1));
+}
+
+//Assign all possible answers in HTML
+    option1.innerHTML = switchAnswers[0];
+    option2.innerHTML = switchAnswers[1];
+    option3.innerHTML = switchAnswers[2];
+}
+
+
+//Add click function to every option and evaluate if the answer is correct
+option1.addEventListener("click", function(){
+    if(option1.innerHTML == answer){  //option1.innerHTML is a STRING so need to be ==, cannot use ===
+        generateEquation();
+    }else{
+        audio.play();
+    }
+})
+
+option2.addEventListener("click", function(){
+    if(option2.innerHTML == answer){
+        generateEquation();
+    }else{
+        audio.play();
+    }
+})
+
+option3.addEventListener("click", function(){
+    if(option3.innerHTML == answer){
+        generateEquation();
+    }else{
+        audio.play();
+    }
+})
+
+//Call the function
+generateEquation();
